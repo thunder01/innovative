74eef1f1b8f2a89d91e35be1279417ebeb805733
chenjing <1427712421@qq.com>
2017-09-29 13:42:19 +0800

    提交

--- innovative/src/main/java/com/innovative/controller/PoiController.java
+++ innovative/src/main/java/com/innovative/controller/PoiController.java
@@ -7,7 +7,6 @@ import com.innovative.utils.JsonResult;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
