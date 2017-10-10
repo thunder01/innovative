148e4ebe6c4457f19e090fec042246aa29f0d9ef
chenjing <1427712421@qq.com>
2017-10-10 17:55:46 +0800

    关于用户修改

--- innovative/src/main/java/com/innovative/bean/User.java
+++ innovative/src/main/java/com/innovative/bean/User.java
@@ -91,17 +91,21 @@ public class User implements Serializable {
 		this.roleIds = roleIds;
 	}
 
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 	public String getUserId() {
 		return userId;
 	}
 	public void setUserId(String userId) {
 		this.userId = userId;
-	}
-	public String getUserName() {
-		return nachn+vorna;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
 	}
 	public String getPernr() {
 		return pernr;
