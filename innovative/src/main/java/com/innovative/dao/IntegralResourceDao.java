ab920c513affd0e1227fe58b3d6481c68bef70b2
hy <305973139@qq.com>
2017-11-02 15:46:15 +0800

    积分模块修改

--- innovative/src/main/java/com/innovative/dao/IntegralResourceDao.java
+++ innovative/src/main/java/com/innovative/dao/IntegralResourceDao.java
@@ -6,22 +6,16 @@ import com.innovative.bean.IntegralResource;
 
 public interface IntegralResourceDao {
 	/**
-	 * 添加一个创新资源详情页进入次数（第一次进来为1）
+	 * 插入一个创新资源详情页查看信息
 	 * @param integralResource
 	 * @return
 	 */
 	public int addIntegralResource(IntegralResource integralResource);
 	/**
-	 * 更新一个创新资源详情页进入次数
-	 * @param integralResource
+	 * 通过类型和资源id来定位资源信息 来查询当前资源被查看的次数
+	 * @param resource_id
+	 * @param type
 	 * @return
 	 */
-	public int updateIntegralResource(IntegralResource integralResource);
-	/**
-	 * 查询资源的访问数量，先查询，访问数量为4则增加20积分，其访问数量置0
-	 * @param type 类型判断是哪个资源
-	 * @param resource_id 资源的id
-	 * @return
-	 */
-	public IntegralResource getIntegralResource(@Param("type") String type,@Param("resource_id") String resource_id);
+	public int getCountByResourceId(String resource_id);
 }
--- innovative/src/main/java/com/innovative/service/IntegralService.java
+++ innovative/src/main/java/com/innovative/service/IntegralService.java
@@ -18,108 +18,113 @@ public class IntegralService {
 	@Resource
 	private IntegralResourceDao integralResourceDao;
 	/**
-	 * 
-	 * @param type 积分类型
+	 * 积分功能 
+	 * @param type 积分类型(1登录、2是资源库详情页若留言、3上传科技信息推特、4科技信息推特中信息的收藏+转发+评论+点赞、5创新资源详情页进入次数
+	 * 、6技术报告的方案下载、7创新资源上传专家、8创新资源上传合作机构、9创新资源上传行业协会、10创新资源上传技术报告、11创新资源上传方案、12创新资源上传仪器设备、
+	 * 13资源库详情页若留言者提供有用信息，寻源工程师可任意打赏50积分	)
 	 * @param userid 用户id
-	 * @param type2 (123456是一期的资源号、7是科技信息推特中信息的收藏+转发+评论+点赞等总数、8是技术报告下载奖励上传人20分)
-	 * @param resource_id 资源的id
+	 * @param resource_id 资源的id(登入的时候把登入的userid发来)
 	 * @return
 	 */
 	@Transactional
-	public void managerIntegral(int type,String userid,String type2,String resource_id){
-		Integral newIntegral = new Integral();
-		newIntegral.setUserid(userid);
-		Integral integral = integralDao.getIntegral(userid);
+	public int managerIntegral(int type,String userid,String resource_id){
+		int result=0;
+		Integral integral = new Integral();
+		integral.setUserid(userid);
+		integral.setType(type);
+		integral.setResource_id(resource_id);
 		switch (type) {
 		case 1://登录： 首次登录获得100分，再次登录获得10分；每日最多2次，封顶20分。
-			if(integral!=null){
-				if(integral.getLogin_count()<2){//判断登入次数是否为2次，每天从0开始
-					integral.setLogin_count(integral.getLogin_count()+1);
-					integral.setIntegral(10);
-					integralDao.updateIntegral(integral);
-				}else {
-					integral.setLogin_count(integral.getLogin_count()+1);
-					integral.setIntegral(0);
-					integralDao.updateIntegral(integral);
-				}
+			if(integralDao.isFirstLogin(userid)==0){
+				integral.setIntegral(110);
+				integral.setContent("首次登入额外获得100分、登入获得10分");
 			}else {
-				integralDao.addIntegral(newIntegral);
+				if(integralDao.todayLoginCount(userid)<2){
+					integral.setContent("登入获得10分");
+					integral.setIntegral(10);
+				}else {
+					integral.setContent("今天第"+integralDao.todayLoginCount(userid)+1+"次登入，不再获得积分");
+					integral.setIntegral(0);
+				}
 			}
+			result = integralDao.addIntegral(integral);
 			break;
-		case 2://留言： 获取20分。资源库详情页若留言者提供有用信息，寻源工程师可任意打赏50积分，（点击一次弹框确认后可以给50分，固定按钮。只能点击一次）。//打赏待做
-			if(integral!=null){
-				integral.setIntegral(20);
-				integralDao.updateIntegral(integral);
-			}
+		case 2://留言： 获取20分。
+			integral.setContent("评论获得20积分");
+			integral.setIntegral(20);
+			result = integralDao.addIntegral(integral);
 			break;
 		case 3://上传科技信息推特：获取20分。
-			if(integral!=null){
-				integral.setIntegral(20);
-				integralDao.updateIntegral(integral);
-			}
+			integral.setContent("上传科技信息推特获得20积分");
+			integral.setIntegral(20);
+			result = integralDao.addIntegral(integral);
 			break;
 		case 4://科技信息推特中信息的收藏+转发+评论+点赞等总数>10获取50分。收藏、转发、评论、点赞最后都得进行积分处理
-			if(integral!=null){
-				IntegralResource integralResource = integralResourceDao.getIntegralResource(type2, resource_id);
-				if(integralResource!=null){
-					if(integralResource.getCount()==10){
-						integral.setIntegral(50);
-						integralDao.updateIntegral(integral);
-					}
-					integralResource.setCount(integralResource.getCount()+1);
-					integralResourceDao.updateIntegralResource(integralResource);
-				}else {
-					IntegralResource ir = new IntegralResource();
-					ir.setResource_id(resource_id);
-					ir.setType(type2);
-					integralResourceDao.addIntegralResource(ir);
-				}
+			int countBySzpd = integralDao.getCountBySzpd(type, resource_id);
+			if(countBySzpd==10){
+				integral.setContent("上传科技信息推特的收藏+转发+评论+点赞总数大于10，获得50积分");
+				integral.setIntegral(50);
+			}else {
+				integral.setIntegral(0);
 			}
+			result =integralDao.addIntegral(integral);
 			break;
-		case 5://创新资源上传专家、合作机构、行业协会、技术报告、方案、仪器设备：获取100分。
-			if(integral!=null){
-				integral.setIntegral(100);
-				integralDao.updateIntegral(integral);
+		case 5://创新资源详情页进入次数>5次，奖励上传人50分。（待定）
+			int count = integralResourceDao.getCountByResourceId(resource_id);
+			if(count==5){
+				integral.setContent("创新资源详情页进入次数超过5次");
+				integral.setIntegral(50);
+			}else {
+				integral.setIntegral(0);
 			}
+			result = integralDao.addIntegral(integral);
 			break;
-		case 6://创新资源详情页进入次数>5次，奖励上传人50分。（待定）
-			if(integral!=null){
-				IntegralResource integralResource = integralResourceDao.getIntegralResource(type2, resource_id);
-				if(integralResource!=null){
-					if(integralResource.getCount()==4){
-						integralResource.setCount(0);//可以累加积分，进入详情页5次就增加50分
-						integralResourceDao.updateIntegralResource(integralResource);
-						integral.setIntegral(50);
-						integralDao.updateIntegral(integral);
-					}else {
-						integralResource.setCount(integralResource.getCount()+1);
-						integralResourceDao.updateIntegralResource(integralResource);
-					}
-				}else {
-					IntegralResource ir = new IntegralResource();
-					ir.setResource_id(resource_id);
-					ir.setType(type2);
-					integralResourceDao.addIntegralResource(ir);
-				}
-			}
+		case 6://技术报告的方案下载奖励上传人20分。
+			integral.setContent("您的方案下载被下载获得20积分");
+			integral.setIntegral(20);
+			result = integralDao.addIntegral(integral);
 			break;
-		case 7://技术报告的方案下载奖励上传人20分。
-			if(integral!=null){
-				IntegralResource integralResource = integralResourceDao.getIntegralResource(type2, resource_id);
-				if(integralResource!=null){
-					integralResource.setCount(integralResource.getCount()+1);
-				}else{
-					IntegralResource ir = new IntegralResource();
-					ir.setResource_id(resource_id);
-					ir.setType(type2);
-					integralResourceDao.addIntegralResource(ir);
-				}
-				integral.setIntegral(20);
-				integralDao.updateIntegral(integral);
-			}
+		//7-12算一个类型的，都是资源创新上传*****************************************************************************	
+		case 7://创新资源上传专家100分
+			integral.setContent("您上传专家获得100积分");
+			integral.setIntegral(100);
+			result = integralDao.addIntegral(integral);
+			break;
+		case 8://创新资源上传合作机构100分
+			integral.setContent("您上传合作机构获得100积分");
+			integral.setIntegral(100);
+			result = integralDao.addIntegral(integral);
+			break;
+		case 9://创新资源上传行业协会获取100分。
+			integral.setContent("您上传行业协会获得100积分");
+			integral.setIntegral(100);
+			result = integralDao.addIntegral(integral);
+			break;
+		case 10://创新资源上传技术报告获取100分。
+			integral.setContent("您上传技术报告获得100积分");
+			integral.setIntegral(100);
+			result = integralDao.addIntegral(integral);
+			break;
+		case 11://创新资源上传方案获取100分。
+			integral.setContent("您上传方案获得100积分");
+			integral.setIntegral(100);
+			result = integralDao.addIntegral(integral);
+			break;
+		case 12://创新资源上传仪器设备获取100分。
+			integral.setContent("您上传仪器设备获得100积分");
+			integral.setIntegral(100);
+			result = integralDao.addIntegral(integral);
+			break;
+		case 13://资源库详情页若留言者提供有用信息，寻源工程师可任意打赏50积分，（点击一次弹框确认后可以给50分，固定按钮。只能点击一次）
+			integral.setContent("您的评论被打赏了50分");
+			integral.setIntegral(50);
+			//**************************************作者同时也应该减少50分
+			/*通过resourc找到上传人，减去50分*/
+			result = integralDao.addIntegral(integral);
 			break;
 		default:
 			break;
 		}
+		return result;
 	}
 }
