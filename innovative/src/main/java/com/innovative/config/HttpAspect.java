29c078cc080d68b71e2507e685ebe0334b7de26f
hy <305973139@qq.com>
2017-11-14 15:46:50 +0800

    日志增加

--- innovative/src/main/java/com/innovative/config/HttpAspect.java
+++ innovative/src/main/java/com/innovative/config/HttpAspect.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -16,6 +17,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.innovative.bean.LoggerRecord;
+import com.innovative.service.LoggerRecordService;
 /**
  * 记录用户行为
  * @author huang
@@ -39,19 +44,21 @@ public class HttpAspect {
     public void doBefore(JoinPoint joinPoint){
     	ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
+        HttpServletResponse response = attributes.getResponse();
         String userid = request.getParameter("userId");
         //URL
         LOGGER.warn("userid={}", userid);
-//        LOGGER.info
-//        LOGGER.warn("URL={}",request.getRequestURL());
-//        //Method
-//        LOGGER.warn("Method={}",request.getMethod());
-//        //IP
-//        LOGGER.warn("IP={}",request.getRemoteAddr());
-//        //Class.Method
-//        LOGGER.warn("CLass.Method={}",joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName()+"()");
-//        //Args
-//        LOGGER.warn("Args={}",joinPoint.getArgs());
+        LoggerRecord loggerRecord= new LoggerRecord();
+        loggerRecord.setUserid(userid);
+        loggerRecord.setMethod(request.getMethod());
+        loggerRecord.setUrl(request.getRequestURL().toString());
+        loggerRecord.setIp(request.getRemoteAddr());
+        loggerRecord.setArgs(joinPoint.getArgs().toString());
+        loggerRecord.setClass_method(joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName()+"()");
+        loggerRecord.setStatus(response.getStatus()+"");
+        LoggerRecordService loggerRecordService = WebApplicationContextUtils.getRequiredWebApplicationContext
+        		(request.getServletContext()).getBean(LoggerRecordService.class);
+        loggerRecordService.addLoggerRecord(loggerRecord);
         Map<String, Object> map = new HashMap<>();
         map.put("userid", userid);
         map.put("url", request.getRequestURL());
