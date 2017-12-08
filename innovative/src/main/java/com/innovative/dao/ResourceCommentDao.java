55599d23bc9f67487248d1620e675fe320650e1a
chenjing <1427712421@qq.com>
2017-12-08 16:51:21 +0800

    tijiao

--- innovative/src/main/java/com/innovative/dao/ResourceCommentDao.java
+++ innovative/src/main/java/com/innovative/dao/ResourceCommentDao.java
@@ -39,13 +39,27 @@ public interface ResourceCommentDao {
 	public int getCountResourceCommentByResourcd_idAndType(@Param("type")Integer type,@Param("resource_id")String resource_id);
 	/**
 	 * 获得当前资源的前5条评论
-	 * @param type
+	 * @param type 类型1专家、2合作机构、3行业协会、4技术报告、5方案、6一起设备
 	 * @param resource_id
 	 * @return
 	 */
 	public List<ResourceComment> getComment(@Param("type")Integer type,@Param("resource_id")String resource_id);
 	
 	public ResourceComment getResourceComment(Integer id);
+	/**
+	 * 
+	 * @param type 类型 类型1专家、2合作机构、3行业协会、4技术报告、5方案、6一起设备
+	 * @param startIndex 
+	 * @param pageSize 
+	 * @return
+	 */
+	public List<ResourceComment> getCommentList(@Param("type")String type, @Param("pageSize")int pageSize, @Param("startIndex")int startIndex);
+	/**
+	 * 
+	 * @param type   类型 类型1专家、2合作机构、3行业协会、4技术报告、5方案、6一起设备
+	 * @return
+	 */
+	public int getCommentListCount(@Param("type")String type);
 	
 	
 }
